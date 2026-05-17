661c340a58b80fd3b10bc195898cdce90ca5e7f5
FKH LOQ15IRX9 <fkh.loqirx9@outlook.com>
2026-05-17 11:34:13 +0700

    change interval to 5000

--- carousel.js
+++ carousel.js
@@ -1,5 +1,5 @@
 var myCarousel = document.querySelector('#whyCarousel');
 var carousel = new bootstrap.Carousel(myCarousel, {
-  interval: 2000, // 4 detik
+  interval: 5000, // 5 detik
   ride: 'carousel'
 });
